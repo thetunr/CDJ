2e4b10112e459b9f6dc25512f021869fa582a0ab
Tony Oh <do256@cornell.edu>
2023-11-29 15:30:02 -0500

    New data

--- heatmap.py
+++ heatmap.py
@@ -5,18 +5,16 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('cdj_dp - corr.csv')
 
-
-#filtered_df = df.drop(columns=['Max Air Temp (˚F)', 'Min Air Temp (˚F)'])
 corr = df.corr()
 plt.subplots(figsize = (16, 8))
 
 #Plot a heatmap of the correlation matrix we created
 #sns.heatmap(corr, annot = True, cmap = 'YlGn')
 
-features1=['Appearance',	'Intelligence', 'Athleticism', 'Humor',	'Optimism',	'Patience',	'Confidence',	'Kindness']
-features2=['Cat1', 'Cat2','num1','num2']
+# features1=['Appearance',	'Intelligence', 'Athleticism', 'Humor',	'Optimism',	'Patience',	'Confidence',	'Kindness']
+# features2=['Cat1', 'Cat2','num1','num2']
 
-corr2 = df[features1].corr()
+# corr2 = df[features1].corr()
 # df[features2].corr()
 
 
